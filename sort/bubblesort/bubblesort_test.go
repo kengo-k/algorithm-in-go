3aec247e60048f0d05f9d2961b444c3bc3ed6da0
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-08-13 23:31:18 +0900

    add bubble sort

---
+++ sort/bubblesort/bubblesort_test.go
@@ -0,0 +1,24 @@
+package bubblesort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	tests := []struct {
+		input  []int
+		output []int
+	}{
+		{[]int{64, 34, 25, 12, 22, 11, 90}, []int{11, 12, 22, 25, 34, 64, 90}},
+		{[]int{5, 1, 4, 2, 8}, []int{1, 2, 4, 5, 8}},
+		{[]int{}, []int{}}, // 空のリスト
+	}
+
+	for _, tt := range tests {
+		bubbleSort(tt.input)
+		if !reflect.DeepEqual(tt.input, tt.output) {
+			t.Errorf("bubbleSort(%v) = %v, want %v", tt.input, tt.input, tt.output)
+		}
+	}
+}
